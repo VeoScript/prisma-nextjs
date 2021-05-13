6d088b105a5eb6dc22dbe7079ba35b7ae41cc79d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-05-13 18:25:55 +0800

    Initial commit

---
+++ pages/api/movies.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async(req, res) => {
+  const data = JSON.parse(req.body)
+  const createdMovie = await prisma.movie.create({ data })
+  res.json(createdMovie)
+}
